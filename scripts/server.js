4cf890a5082d2e175b5133d1111e2435e4953ab7
djfm <fm.de.jouvencel@gmail.com>
2015-07-25 10:35:26 +0200

    // generic message

--- scripts/server.js
+++ scripts/server.js
@@ -7,5 +7,5 @@ app.use(express.static('dist'));
 var server = app.listen(process.env.PORT || process.argv[2] || 3000, function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log('no-go app listening on http://%s:%s', host, port);
+    console.log('Listening on http://%s:%s', host, port);
 });
